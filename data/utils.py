5d9fd416c97761f2e809d1561196b18c8d68250a
Neubauer-A <67448788+Neubauer-A@users.noreply.github.com>
2021-05-27 20:03:57 -0500

    Update utils.py

--- data/utils.py
+++ data/utils.py
@@ -12,12 +12,12 @@ def write_raw_set(filename, max_lines):
     f = open(f'{filename}_{count}.py', 'w')
     f.write('d = {\n')
     for i in tweet_set:
-      count += 1
-      f.write('"'+i.strip()+'": 0,'+'\n')
-      if count % max_lines == 0:
-        f.write('}')
-        f.close()
-        f = open(f'{filename}_{count}.py', 'w')
-        f.write('d = {\n')
+        count += 1
+        f.write('"'+i.strip()+'": 0,'+'\n')
+        if count % max_lines == 0:
+            f.write('}')
+            f.close()
+            f = open(f'{filename}_{count}.py', 'w')
+            f.write('d = {\n')
     f.write('}')
     f.close()
